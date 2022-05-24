8424eeefc686bdb75b992d5a7ef521a5dbe75f47
jhosef1234 <jhosefqh123@gmail.com>
2022-05-24 15:10:56 +0000

    clase10_SubProgramas

---
+++ clase10_SubProgramas/Aritmetica.java
@@ -0,0 +1,21 @@
+package clase10_SubProgramas;
+
+public class Aritmetica {
+    int va, vb;
+
+    Aritmetica(){
+    }
+
+    Aritmetica(int va, int vb){
+        this.va=va;
+        this.vb=vb;
+    }
+
+    public int sumar(int a, int b) {
+        return a+b;
+    }
+
+    public int multiplicar(){
+        return (va*vb);
+    }
+}
